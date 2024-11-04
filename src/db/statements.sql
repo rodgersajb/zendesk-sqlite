9852b85614926e7973a4c3f1fe81aab823ea29ba
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2024-11-04 13:21:43 -0500

    set up sqlite db, create user table, ran a few commands

---
+++ src/db/statements.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER 
+)
+
+INSERT INTO users (name, email, age) 
+VALUES 
+    ('Alice', 'tremendous.lemur.wckz@flashpost.net', 25),
+    ('Bob', 'rwz0s@livinitlarge.net', 30),
+    ('Charlie', 'colohek646@operades.com', 35)
